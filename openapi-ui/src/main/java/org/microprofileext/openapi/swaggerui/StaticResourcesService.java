463dac19d13df66379e10d5e3f2b9949a502e625
Frank Eichfelder <frank.eichfelder@softceed.de>
2020-02-13 10:11:55 +0100

    Fix for static resources

--- openapi-ui/src/main/java/org/microprofileext/openapi/swaggerui/StaticResourcesService.java
+++ openapi-ui/src/main/java/org/microprofileext/openapi/swaggerui/StaticResourcesService.java
@@ -1,15 +1,11 @@
 package org.microprofileext.openapi.swaggerui;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
+import java.io.*;
 import java.util.logging.Level;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
 import lombok.extern.java.Log;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 
@@ -24,25 +20,31 @@ public class StaticResourcesService {
     @GET
     @Operation(hidden = true)
     public Response staticJsResources(@PathParam("path") final String path) {
-        try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(String.format("META-INF/resources/%s", path));
-                BufferedReader buffer = new BufferedReader(new InputStreamReader(inputStream));
-                StringWriter stringWriter = new StringWriter()) {
-            buffer.lines().forEach(stringWriter::write);
-
-            String response = stringWriter.toString();
-            if (response != null && !response.isEmpty()) {
-                Response.ResponseBuilder ret = Response.ok(response);
-                if (path.endsWith(".js")) {
-                    ret = ret.type("application/javascript");
+        StreamingOutput result = new StreamingOutput() {
+            @Override
+            public void write(OutputStream output)
+                    throws WebApplicationException {
+                try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(String.format("META-INF/resources/%s", path))) {
+                    byte[] buffer = new byte[1024];
+                    int len = inputStream.read(buffer);
+                    while (len != -1) {
+                        output.write(buffer, 0, len);
+                        len = inputStream.read(buffer);
+                    }
+                } catch (IOException ex) {
+                    log.severe(ex.getMessage());
+                    throw new InternalServerErrorException(ex);
+                } catch (NullPointerException ex) {
+                    log.log(Level.WARNING, "Could not find resource [{0}]", path);
+                    throw new NotFoundException();
                 }
-                return ret.build();
             }
-        } catch (IOException ex) {
-            log.severe(ex.getMessage());
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        };
+        Response.ResponseBuilder ret = Response.ok(result);
+        if (path.endsWith(".js")) {
+            ret = ret.type("application/javascript");
         }
-        log.log(Level.WARNING, "Could not find resource [{0}]", path);
-        return Response.status(Response.Status.NOT_FOUND).build();
+        return ret.build();
     }
     
 }
