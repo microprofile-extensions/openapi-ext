6324be467e075a8f713b879dac818bc462ccc108
deepakr1618 <deepak.r1618@gmail.com>
2025-01-18 16:56:02 +0530

    Swagger UI upgrade and changes to log messages

--- openapi-ui/src/main/java/org/microprofileext/openapi/swaggerui/StaticResourcesService.java
+++ openapi-ui/src/main/java/org/microprofileext/openapi/swaggerui/StaticResourcesService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.logging.Level;
 import lombok.extern.java.Log;
 import org.eclipse.microprofile.openapi.annotations.Operation;
@@ -25,25 +26,30 @@ public class StaticResourcesService {
     @GET
     @Operation(hidden = true)
     public Response staticJsResources(@PathParam("path") final String path) {
-        try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(String.format("META-INF/resources/%s", path));
-                BufferedReader buffer = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(String.format("META-INF/resources/%s", path))) {
+            if(Objects.isNull(inputStream)) {
+                log.log(Level.WARNING, "Could not find resource [{0}]", path);
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+            try ( BufferedReader buffer = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
                 StringWriter stringWriter = new StringWriter()) {
-            buffer.lines().forEach(stringWriter::write);
-
-            String response = stringWriter.toString();
-            if (response != null && !response.isEmpty()) {
-                Response.ResponseBuilder ret = Response.ok(response);
-                if (path.endsWith(".js")) {
-                    ret = ret.type("application/javascript");
+                buffer.lines().forEach(stringWriter::write);
+                String response = stringWriter.toString();
+                if (response != null && !response.isEmpty()) {
+                    Response.ResponseBuilder ret = Response.ok(response);
+                    if (path.endsWith(".js")) {
+                        ret = ret.type("application/javascript");
+                    }
+                    return ret.build();
+                } else {
+                    log.log(Level.WARNING, "The requested swagger file is empty!");
+                    return Response.noContent().build();
                 }
-                return ret.build();
             }
         } catch (IOException | NullPointerException ex) {
-            log.severe(ex.getMessage());
+            log.log(Level.SEVERE, "Failed to read the static Swagger file at path: {0}. Error details: {1}", new Object[]{path, ex.getMessage()});
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
-        log.log(Level.WARNING, "Could not find resource [{0}]", path);
-        return Response.status(Response.Status.NOT_FOUND).build();
     }
     
 }
